522c7406b7cb17cff4d1556cc9ef213c8a89781d
xkvbikx <xkvbikx@gmail.com>
2023-04-06 13:17:04 +0200

    add image capture

--- desktop_app/src/renderer/src/components/camera/CameraPanel.tsx
+++ desktop_app/src/renderer/src/components/camera/CameraPanel.tsx
@@ -10,23 +10,43 @@ type Params = {
 function Camera({ cameraId }: Params): JSX.Element {
   useEffect(() => {
     startCamera()
+    setInterval(() => {
+      takepicture()
+    }, 1000)
   }, [])
 
   const startCamera = async (): Promise<void> => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
         deviceId: { exact: cameraId },
-        frameRate: { ideal: 60 }
+        frameRate: { ideal: 60 },
+        width: { ideal: 4096 },
+        height: { ideal: 2160 }
       }
     })
     const video: HTMLVideoElement = document.querySelector('#camera')!
     video!.srcObject = stream
+    video.play()
     toast('Camera started', { icon: '📷' })
+  }
+
+  const takepicture = (): void => {
+    const video = document.getElementById('camera')! as HTMLVideoElement
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement
+
+    const { height, width } = video.getBoundingClientRect()
+    const context = canvas.getContext('2d')!
+
+    canvas.width = width
+    canvas.height = height
+    context.drawImage(video, 0, 0, width, height)
+    const captureImage = canvas.toDataURL('image/png')
   }
 
   return (
     <Stack justifyContent="center" sx={{ height: '100vh' }}>
-      <CardMedia id="camera" component="video" autoPlay sx={{ maxHeight: '100vh' }} />
+      <CardMedia id="camera" component="video" sx={{ maxHeight: '100%' }} />
+      <canvas id="canvas" style={{ display: 'none' }}></canvas>
     </Stack>
   )
 }
